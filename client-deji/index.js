1158f4a97e31a7d5ab18decb81b4fa3402725a99
github username <github email address>
2019-02-13 15:54:24 +0000

    front end fetches and gameplay

--- client-deji/index.js
+++ client-deji/index.js
@@ -1,10 +1,11 @@
 const canvas = document.querySelector('#gameCanvas')
 let canvasContext = canvas.getContext('2d')
+// let canvasContext.font = "30px Verdana";
 let ballX = 50
 let ballY = 50
 let ballSpeedX = 20
 let ballSpeedY = 4
-let framesPerSecond = 10
+let framesPerSecond = 30
 // let paddleOneY = 250
 let paddleOneHeight = 100
 let paddleOneThickness = 10
@@ -13,12 +14,19 @@ let paddleTwoHeight = 100
 let paddleTwoThickness = 10
 let playerOneScore = 0
 let playerTwoScore = 0
-const winningScore = 7
+const winningScore = 10
 let showingWinScreen = false
 let SERVER = 'http://10.218.2.156:3000/state'
+const URL = 'http://localhost:3000/games'
+// let state.duration = 0;
+let sent = false
 
 const state = {
-    paddleOneY: 250
+  paddleOneY: 250,
+  userId: null,
+  duration: 0,
+  games: null,
+  users: null
 }
 // the width of the ball can be changed over time to make the game more difficult
 
@@ -33,28 +41,66 @@ function calculateMousePosition (evt) {
   }
 }
 
-function drawNet() {
-    for (let i = 0; i < canvas.height; i += 40) {
-        colorRect(canvas.width/2 - 1 , i, 2, 20, 'white')
-    }
+function renderScors() {
+  
 }
-function drawEverything (evt) {
-  // draw the canvas
+
+function getGames () {
+  return fetch('http://localhost:3000/games')
+    .then(resp => resp.json())
+    .then(games => (state.games = games))
+}
+
+function getUsers () {
+  return fetch('http://localhost:3000/users')
+    .then(resp => resp.json())
+    .then(users => (state.users = users))
+}
+
+function winningScreen () {
   colorRect(0, 0, canvas.width, canvas.height, 'black')
   if (showingWinScreen) {
     canvasContext.fillStyle = 'white'
+    //   fetch(URL, {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({user_id: 3, score: 0 })
+    //  })
 
-    if (playerOneScore >= winningScore) {
-      canvasContext.fillText('Left player won!!!', 350, 200)
-    } else if (playerTwoScore >= winningScore) {
-      canvasContext.fillText('Right player won!!!', 350, 200)
+    // if (playerOneScore >= winningScore) {
+    //   canvasContext.fillText('Left player won!!!', 350, 200)
+    // } else
+    if (!sent) {
+      sendUserScore()
+        .then(getGames)
+        .then(getUsers)
+      sent = true
     }
-    console.log(playerOneScore)
-    console.log(playerTwoScore)
-    canvasContext.fillText('click to continue', 350, 500)
 
-    return
+    if (playerTwoScore >= winningScore) {
+      canvasContext.fillText(
+        `Game Over! You lasted ${Math.round(state.duration)} seconds!`,
+        350,
+        200
+      )
+    }
+
+    canvasContext.fillText('click to continue', 350, 500)
   }
+}
+
+function drawNet () {
+  for (let i = 0; i < canvas.height; i += 40) {
+    colorRect(canvas.width / 2 - 1, i, 2, 20, 'white')
+  }
+}
+function drawEverything (evt) {
+  // draw the canvas
+
+  // paddleTwoHeight =  if (state.duration > 10) {
+
+  // } ? canvas.height : paddleTwoHeight
+
   drawNet()
   //   draw the left player paddle
   colorRect(0, state.paddleOneY, paddleOneThickness, paddleOneHeight, 'white')
@@ -70,8 +116,15 @@ function drawEverything (evt) {
 
   //   draw the ball
   colorCircle(ballX, ballY, 10, 'white')
-  canvasContext.fillText(playerOneScore, 100, 100)
-  canvasContext.fillText(playerTwoScore, canvas.width - 100, 100)
+  // canvasContext.fillText(playerOneScore, 100, 100)
+  // show the scores
+  canvasContext.fillStyle = 'blue'
+  // ctx.font = "30px Verdana";
+  canvasContext.fillText(
+    `You have ${10 - playerTwoScore} lives left`,
+    canvas.width - 550,
+    100
+  )
 }
 
 function colorRect (leftX, topY, width, height, drawColor) {
@@ -106,7 +159,10 @@ function moveEverything () {
   //   paddleTwoHeight += 1
 
   if (ballX <= 0) {
-    if (ballY > state.paddleOneY && ballY < state.paddleOneY + paddleOneHeight) {
+    if (
+      ballY > state.paddleOneY &&
+      ballY < state.paddleOneY + paddleOneHeight
+    ) {
       ballSpeedX = -ballSpeedX
       let diff = ballY - (state.paddleOneY + paddleOneHeight / 2)
       ballSpeedY = diff * 0.35
@@ -135,10 +191,8 @@ function moveEverything () {
 }
 
 function ballReset () {
-  if (playerOneScore >= winningScore || playerTwoScore >= winningScore) {
-
+  if (playerTwoScore >= winningScore) {
     showingWinScreen = true
-
   }
   ballSpeedX = -ballSpeedX
   ballX = canvas.width / 2
@@ -150,6 +204,7 @@ function handleMouseClick () {
     playerOneScore = 0
     playerTwoScore = 0
     showingWinScreen = false
+    state.duration = 0
   }
 }
 
@@ -166,16 +221,50 @@ function handleMouseClick () {
 //
 // }
 
+function handleFormSubmit () {
+  document.querySelector('form').addEventListener('submit', () => {
+    event.preventDefault()
+    sent = false
+    initalize()
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: event.target.name.value })
+    })
+      .then(resp => resp.json())
+      .then(user => (state.userId = user.id))
+  })
+}
 
+function sendUserScore () {
+  return fetch('http://localhost:3000/games', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      user_id: state.userId,
+      score: Math.floor(state.duration)
+    })
+  })
+    .then(resp => resp.json())
+    .then(console.log)
+}
 
 function initalize () {
   // setInterval(updateState, 1000/10)
+
   setInterval(() => {
-    drawEverything()
-    moveEverything()
+    if (!showingWinScreen) {
+      state.duration += 1 / framesPerSecond
+    }
+
+    winningScreen()
+
+    if (!showingWinScreen) {
+      drawEverything()
+      moveEverything()
+    }
   }, 1000 / framesPerSecond)
   document.addEventListener('keydown', function (evt) {
-    console.log(event.key)
     if (event.key === 'ArrowUp') {
       state.paddleOneY -= 10
     }
@@ -187,10 +276,9 @@ function initalize () {
   canvas.addEventListener('mousemove', function (evt) {
     let mousePos = calculateMousePosition(evt)
     state.paddleOneY = mousePos.y - paddleOneHeight / 2
-
   })
 
   canvas.addEventListener('click', handleMouseClick)
 }
 
-initalize()
+handleFormSubmit()
